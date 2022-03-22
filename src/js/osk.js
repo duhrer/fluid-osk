1ee6a0088f90a0b05f55b817be491bc96e497804
Tony Atkins <duhrer@gmail.com>
2022-03-22 18:47:52 +0100

    GH-1: Eliminated need for rerender.

--- src/js/osk.js
+++ src/js/osk.js
@@ -4,46 +4,21 @@
 
     fluid.defaults("osk.templateRenderer", {
         gradeNames: ["fluid.containerRenderingView"],
-        excludedOptions: ["excludedOptions", "model", "invokers", "listeners", "modelListeners", "members", "components", "template"],
-        events: {
-            afterRerender: null
-        },
-
         markup: {
             container: ""
         },
         invokers: {
-            getClassnames: {
-                funcName: "fluid.identity",
-                args: [[]]
-            },
             renderMarkup: {
                 funcName: "osk.templateRenderer.render",
-                args: ["{that}", "{that}.options.markup"]
-            },
-            reRender: {
-                funcName: "osk.templateRenderer.reRender",
-                args: ["{that}"]
+                args: ["{that}", "{that}.options.markup.container", "{that}.model"]
             }
         }
     });
 
-    osk.templateRenderer.render = function (that, markupTemplate) {
-        var classNames= that.getClassnames();
-        var sanitisedPayload = {
-            classNames: classNames,
-            model: that.model
-        };
-
-        var renderedContent = fluid.stringTemplate(that.options.markup.container, sanitisedPayload);
+    osk.templateRenderer.render = function (that, markupTemplate, model) {
+        var renderedContent = fluid.stringTemplate(markupTemplate, model);
         return renderedContent;
     };
-
-    osk.templateRenderer.reRender = function (that) {
-        that.container.remove();
-        that.container = that.renderContainer();
-        that.events.afterRerender.fire();
-    }
 
     // TODO: Make key bindings actually do something
     // TODO: Mouse bindings
@@ -52,7 +27,7 @@
     fluid.defaults("osk.key", {
         gradeNames: ["osk.templateRenderer"],
         markup: {
-            container: "<div class='%classNames'>%model.label</div>\n"
+            container: "<div class='osk-key'>%label</div>\n"
         },
 
         // TODO: Define a sensible definition for multiple modes and keyboard event payloads
@@ -72,46 +47,35 @@
             isDeactivated: false
         },
         modelListeners: {
-            isDown:        { func: "{that}.reRender", excludeSource: "init" },
-            isDeactivated: { func: "{that}.reRender", excludeSource: "init" },
+            // https://api.jquery.com/toggleClass/#toggleClass-className-state
+            // .toggleClass( classNames, state )
+            isDown: {
+                excludeSource: "init",
+                this: "{that}.container",
+                method: "toggleClass",
+                args: ["osk-key-isDown", "{change}.value"], // classNames, state
+            },
+            isDeactivated: {
+                excludeSource: "init",
+                this: "{that}.container",
+                method: "toggleClass",
+                args: ["osk-key-isDeactivated", "{change}.value"], // classNames, state
+            }
         },
         invokers: {
-            getClassnames: {
-                funcName: "osk.key.getClassnames",
-                args: ["{that}"]
-            },
             handleClick: {
                 funcName: "console.log",
                 args: ["click"]
             }
         },
         listeners: {
-            // We have to create separate bindings for the initial render and rerendering, because if we trigger
-            // "afterRender" during the onCreate, we get warnings about a circular reference to the container.
             "onCreate.bindClick": {
-                "this": "{that}.container",
-                "method": "click",
-                "args": ["{that}.handleClick"]
-            },
-            "afterRerender.bindClick": {
                 "this": "{that}.container",
                 "method": "click",
                 "args": ["{that}.handleClick"]
             }
         }
     });
-
-    osk.key.getClassnames = function (that) {
-        var classNames = ["osk-key"];
-
-        fluid.each(["isDown", "isDeactivated"], function (modelValueKey) {
-            if (fluid.get(that.model, modelValueKey)) {
-                classNames.push("osk-key-" + modelValueKey);
-            }
-        });
-
-        return classNames.join(" ");
-    }
 
     fluid.defaults("osk.row", {
         gradeNames: ["osk.templateRenderer"],
